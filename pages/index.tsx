3c847392b7d7be170279128c6d288ec46dc3f7df
RyogaTakaoka <takaoka7pg@gmail.com>
2023-01-23 18:55:40 +0900

    Add filtering function
    Filter機能追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,17 @@ import { useState } from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home(){
+  //ToDoの型
   type todo = {
     id: number
     taskName: string
-    isCompleted: boolean
     detail: string
+    isCompleted: boolean
+    isRemoved:boolean
   }
+
+  //Filterの型
+  type Filter = 'all' | 'checked' | 'unchecked' | 'removed';
 
   //taskname
   const [text, setText] = useState("")
@@ -23,26 +28,56 @@ export default function Home(){
   //todo[] todoList;
   const [todoList, setTodoList] = useState<todo[]>([])
 
+  const [filter, setFilter] = useState<Filter>('all');
+
   //todo newTodo;
   const newTodo:todo = {
     id:todoList.length,
     taskName:text,
+    detail:detail,
     isCompleted:false,
-    detail:detail
+    isRemoved:false
   }
 
+  //Firter処理したToDoリスト
+  const filteredTodoList = todoList.filter((todo) => {
+    // filter ステートの値に応じて異なる内容の配列を返す
+    switch (filter) {
+      case 'all':
+        //削除していないタスク全て
+        return !todo.isRemoved;
+      case 'checked':
+        //完了かつ削除していないタスク
+        return todo.isCompleted && !todo.isRemoved;
+      case 'unchecked':
+        //未完了かつ削除していないタスク
+        return !todo.isCompleted && !todo.isRemoved;
+      case 'removed':
+        //削除したタスク
+        return todo.isRemoved;
+      default:
+        return todo;
+    }
+  });
+
+  //タスク追加処理
   const onClickAddTask = () => {
+    //空は返す
     if(text === '') return 
     
+    //inputの中身リセット
     setText("")
     setDetail("")
     
+    //新規タスクを追加
     if(newTodo!=undefined&&todoList!=undefined){
       setTodoList([...todoList,newTodo])
     }
   }
 
+  //タスク完了・未完了処理
   const handleIsCompleted = (id:number, isCompleted:boolean) => {
+    //const deepCopy = todoList.map((todo) => ({ ...todo }));
     const newTodos = todoList.map((todo) => {
       if(todo.id === id){
         todo.isCompleted = !isCompleted
@@ -53,8 +88,15 @@ export default function Home(){
     setTodoList(newTodos)
   }
 
-  const handleDeleted = (id:number) => {
-    const newTodos = todoList.filter((todo) => todo.id !== id)
+  //タスク削除処理
+  const handleRemoved = (id:number, isRemoved:boolean) => {
+    //const deepCopy = todoList.map((todo) => ({ ...todo }));
+    const newTodos = todoList.map((todo) => {
+      if(todo.id === id){
+        todo.isRemoved = !isRemoved
+      }
+      return todo
+    })
     setTodoList(newTodos)
   }
 
@@ -71,12 +113,11 @@ export default function Home(){
         break;
     }
   };
-  
 
   return (
     <div className={styles.body}>
 
-      <h1>ToDo List</h1>
+      <div className={styles.title}>ToDo List</div>
       <div className={styles.inputField}>
 
         <div>
@@ -100,32 +141,46 @@ export default function Home(){
           />
         </div>
         <button onClick={onClickAddTask}>追加</button>
+
+      </div>
+
+      <div>
+        <select
+          defaultValue="all"
+          onChange={(e) => setFilter(e.target.value as Filter)}
+        >
+          <option value="all">すべてのタスク</option>
+          <option value="unchecked">未完了タスク</option>
+          <option value="checked">完了タスク</option>
+          <option value="removed">削除したタスク</option>
+        </select>
       </div>
 
       {/* <p>入力中：{text}</p> */}
 
       {/* 配列の要素を列挙 */}
-      {todoList?.map((todo,index)=>(
-      <div  className={styles.todoList} key={index}>
+      {filteredTodoList?.map((todo)=>(
+      <div  className={styles.todoList} key={todo.id}>
+
+        <input type={"checkbox"} className={styles.todoText}
+          checked={todo.isCompleted}
+          onChange={() => handleIsCompleted(todo.id,todo.isCompleted)}
+        />
 
         <div className={styles.todoText}>
           {'タスク名：'}{todo.taskName}
         </div>
 
         <div className={styles.todoText}>
+          {'詳細：'}{todo.detail}
+        </div>
+        
+        <div className={styles.todoText}>
           {'状態：'}{todo.isCompleted?"完了":"未完了"}
         </div>
 
-        <div className={styles.todoText}>
-          {'詳細：'}{todo.detail}
-        </div>
-
-        <input type={"checkbox"} className={styles.todoText}
-          onChange={(e) => handleIsCompleted(index,todo.isCompleted)}
-        />
-
         <button className={styles.todoText}
-          onClick={() => handleDeleted(todo.id)}>Delete</button>
+          onClick={() => handleRemoved(todo.id,todo.isRemoved)}>{todo.isRemoved?"復元":"削除"}</button>
 
       </div>))}
       
