35b2fd1117f51a1e3e477ec08e661487bac54941
RyogaTakaoka <takaoka7pg@gmail.com>
2023-01-23 09:56:57 +0900

    Add button
    完了ボタンと削除ボタンの実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,46 +8,60 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home(){
   type todo = {
-    taskNames: string
+    id: number
+    taskName: string
     isCompleted: boolean
-    details: string
+    detail: string
   }
 
-  const name: number= 1
   //taskname
   const [text, setText] = useState("")
-  const [addText, setAddText] = useState("")
 
   //detail
   const [detail, setDetail] = useState("")
-  const [addDetail, setAddDetail] = useState("")
-
-  //todo todoTask;
-  //const [todoTask, setTodoList] = useState<todo>()
 
   //todo[] todoList;
-  const [todoList, addTodoList] = useState<todo[]>([])
+  const [todoList, setTodoList] = useState<todo[]>([])
 
-  // const addTodoList = () =>{
-  //   setTodoList([...todoList,todoTask])
-  // }
+  //todo newTodo;
+  const newTodo:todo = {
+    id:todoList.length,
+    taskName:text,
+    isCompleted:false,
+    detail:detail
+  }
 
   const onClickAddTask = () => {
     if(text === '') return 
     
     setText("")
     setDetail("")
-
-    const todoTask:todo = {taskNames:text, isCompleted:false, details:detail}
     
-    if(todoTask!=undefined&&todoList!=undefined){
-      addTodoList([...todoList,todoTask])
+    if(newTodo!=undefined&&todoList!=undefined){
+      setTodoList([...todoList,newTodo])
     }
   }
 
-  const [composing, setComposition] = useState(false);
-  const startComposition = () => setComposition(true);
-  const endComposition = () => setComposition(false);
+  const handleIsCompleted = (id:number, isCompleted:boolean) => {
+    const newTodos = todoList.map((todo) => {
+      if(todo.id === id){
+        todo.isCompleted = !isCompleted
+      }
+      return todo
+    })
+
+    setTodoList(newTodos)
+  }
+
+  const handleDeleted = (id:number) => {
+    const newTodos = todoList.filter((todo) => todo.id !== id)
+    setTodoList(newTodos)
+  }
+
+  //Enter入力
+  const [composing, setComposition] = useState(false)
+  const startComposition = () => setComposition(true)
+  const endComposition = () => setComposition(false)
 
   const onKeydown = (key: string) => {
     switch (key) {
@@ -61,18 +75,23 @@ export default function Home(){
 
   return (
     <div className={styles.body}>
+
+      <h1>ToDo List</h1>
       <div className={styles.inputField}>
-        <div className={styles.inputGenre}>
+
+        <div>
           <p>タスク名</p>
           <p>タスク詳細</p>
         </div>
+
         <div>
           <input type={"text"}
             value={text}
             onChange={(event) => setText(event.target.value)}
-            placeholder="タスクを入力"
+            placeholder="タスク名を入力"
           /> 
           <br/>
+          
           <input type={"text"}
             value={detail}
             onChange={(event) => setDetail(event.target.value)}
@@ -80,25 +99,34 @@ export default function Home(){
             onKeyDown={(e) => onKeydown(e.key)}
           />
         </div>
+        <button onClick={onClickAddTask}>追加</button>
       </div>
-      
-      <button onClick={onClickAddTask}>追加</button>
 
       {/* <p>入力中：{text}</p> */}
-      {/* <p>追加：{todoList![0].taskNames}</p> */}
 
       {/* 配列の要素を列挙 */}
       {todoList?.map((todo,index)=>(
       <div  className={styles.todoList} key={index}>
+
         <div className={styles.todoText}>
-          {'タスク名：'}{todo.taskNames}
+          {'タスク名：'}{todo.taskName}
         </div>
+
         <div className={styles.todoText}>
           {'状態：'}{todo.isCompleted?"完了":"未完了"}
         </div>
+
         <div className={styles.todoText}>
-          {'詳細：'}{todo.details}
+          {'詳細：'}{todo.detail}
         </div>
+
+        <input type={"checkbox"} className={styles.todoText}
+          onChange={(e) => handleIsCompleted(index,todo.isCompleted)}
+        />
+
+        <button className={styles.todoText}
+          onClick={() => handleDeleted(todo.id)}>Delete</button>
+
       </div>))}
       
       {console.log(todoList)}
